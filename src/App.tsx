cb1c55503679cfff17544d3c862b9b0f125d43b5
amy liu <amyliu45@gmail.com>
2026-08-11 21:19:15 -0600

    add dblclick event

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import type { LatLngExpression } from 'leaflet';
+import { circle, type LatLngExpression } from 'leaflet';
 import './App.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMap } from 'react-leaflet'
+
+function DblClickEvent() {
+  const map = useMap();
+  useMapEvent('dblclick', (event) => {
+    const coords = event.latlng;
+    circle(coords, { radius: 20, color: 'red', weight: 1 }).addTo(map);
+  })
+  return null
+}
+
 
 function App() {
   const mapCenter: LatLngExpression = [49.262861, -123.251882]; // Earth sciences
@@ -9,16 +19,21 @@ function App() {
   const csBuilding: LatLngExpression = [49.26140020453208, -123.248899116439];
   const wreckBeach: LatLngExpression = [49.262224, -123.261646];
   const moa: LatLngExpression = [49.269445, -123.259509];
-  
+
   return (
     <>
-      <h2>Map of UBC places...</h2>
+      <h2>Map of UBC Places</h2>
       <MapContainer id='leaflet-map' center={mapCenter} zoom={15}>
         {/* OpenStreetMap overlay */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        {/* Events */}
+        <DblClickEvent></DblClickEvent>
+
+        {/* Markers */}
         <Marker position={marineDriveCoords}>
           <Popup>
             Marine Drive residence, building 4. <br /> A beautiful place.
