14bf0bc009156189a8b7169c4df744a248b44117
amy liu <amyliu45@gmail.com>
2026-08-11 21:10:21 -0600

    add map with some locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import type { LatLngExpression } from 'leaflet';
 import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const mapCenter: LatLngExpression = [49.262861, -123.251882]; // Earth sciences
+  const marineDriveCoords: LatLngExpression = [49.261442, -123.255066];
+  const totemParkCoords: LatLngExpression = [49.259609, -123.252386];
+  const csBuilding: LatLngExpression = [49.26140020453208, -123.248899116439];
+  const wreckBeach: LatLngExpression = [49.262224, -123.261646];
+  const moa: LatLngExpression = [49.269445, -123.259509];
+  
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <h2>Map of UBC places...</h2>
+      <MapContainer id='leaflet-map' center={mapCenter} zoom={15}>
+        {/* OpenStreetMap overlay */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={marineDriveCoords}>
+          <Popup>
+            Marine Drive residence, building 4. <br /> A beautiful place.
+          </Popup>
+        </Marker>
+        <Marker position={totemParkCoords}>
+          <Popup>
+            Totem Park residence, Kwakiutl House. <br /> Lots of good memories.
+          </Popup>
+        </Marker>
+        <Marker position={csBuilding}>
+          <Popup>
+            ICICS/CS Building. <br /> Yes, the name is that long.
+          </Popup>
+        </Marker>
+        <Marker position={wreckBeach}>
+          <Popup>
+            Wreck Beach. <br /> Beware, this beach is nude-allowed.
+          </Popup>
+        </Marker>
+        <Marker position={moa}>
+          <Popup>
+            Museum of Anthropoligy. <br /> A must-visit.
+          </Popup>
+        </Marker>
+      </MapContainer>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
